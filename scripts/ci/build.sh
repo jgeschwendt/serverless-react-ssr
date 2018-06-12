47150edac72eb412e76d496b9262a6d1f4696f91
jlg <joshua@geschwendt.com>
2018-06-12 12:33:48 -0400

    add the rest

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -3,7 +3,11 @@
 set -u -e -o pipefail
 
 docker run \
+  --env ARTIFACTS_BUCKET=$ARTIFACTS_BUCKET \
+  --env API_DOMAIN_NAME=$API_DOMAIN_NAME \
+  --env APP_DOMAIN_NAME=$APP_DOMAIN_NAME \
   --env APP_HOSTED_ZONE=$APP_HOSTED_ZONE \
+  --env APP_SSL_CERT_ARN=$APP_SSL_CERT_ARN \
   --rm \
   --tty \
   --volume $TRAVIS_BUILD_DIR:/var/task \
@@ -11,7 +15,11 @@ docker run \
   $CONTAINER_NAME yarn run build:static
 
 docker run \
+  --env ARTIFACTS_BUCKET=$ARTIFACTS_BUCKET \
+  --env API_DOMAIN_NAME=$API_DOMAIN_NAME \
+  --env APP_DOMAIN_NAME=$APP_DOMAIN_NAME \
   --env APP_HOSTED_ZONE=$APP_HOSTED_ZONE \
+  --env APP_SSL_CERT_ARN=$APP_SSL_CERT_ARN \
   --rm \
   --tty \
   --volume $TRAVIS_BUILD_DIR:/var/task \
